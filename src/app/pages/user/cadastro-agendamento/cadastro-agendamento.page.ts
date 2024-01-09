931e0ab717391e07d43d47aad615117584bc3dcd
italo <italorodridev@gmail.com>
2024-01-08 23:10:17 -0300

    ajuste form cadastro agendamento

--- src/app/pages/user/cadastro-agendamento/cadastro-agendamento.page.ts
+++ src/app/pages/user/cadastro-agendamento/cadastro-agendamento.page.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ModalController } from '@ionic/angular/standalone';
 import * as moment from 'moment';
 import { AgendamentoModel } from 'src/app/models/agendamento.model';
+import { ApartamentoModel } from 'src/app/models/apartamento.model';
 import { AlertsService } from 'src/app/services/alerts/alerts.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { FormService } from 'src/app/services/forms/form.service';
 import { AgendamentoAreaGourmetService } from 'src/app/services/user/agendamento-area-gourmet.service';
 
@@ -17,14 +19,17 @@ import { AgendamentoAreaGourmetService } from 'src/app/services/user/agendamento
   imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class CadastroAgendamentoPage implements OnInit {
+  dataApt: ApartamentoModel | undefined;
   form: FormGroup = this.formService.formAgendamento;
   listAgendamentos: Array<AgendamentoModel> = [];
   listDatasOcupadas: Array<any> = [];
   datasLivres: Array<any> = [];
   mesInicial = moment().add(1, 'day').format();
   mesLimite = moment().add(1, 'month').format();
+  disabledItemUser: boolean = false;
 
   constructor(
+    private authService: AuthService,
     private service: AgendamentoAreaGourmetService,
     private formService: FormService,
     private modalCtrl: ModalController,
@@ -40,6 +45,18 @@ export class CadastroAgendamentoPage implements OnInit {
   }
 
   getDataService() {
+    this.authService.bsDataApt.subscribe((data: ApartamentoModel) => {
+      if (data != null) {
+        this.form.patchValue({
+          nomeResponsavel: data.nome,
+          apartamento: data.numeroBloco,
+        });
+        this.disabledItemUser = true;
+      } else {
+        this.disabledItemUser = false;
+      }
+    });
+
     this.service.listItems.subscribe((list) => {
       this.listAgendamentos = list;
       this.addDaysToCalendar();
